22d90f07a11a0b96444c67c08a495be81b47379c
Rafael_Karosuo <rafaelkarosuo@gmail.com>
2016-09-23 11:08:34 -0700

    Template de sensado, cada 5 minutos

---
+++ sensado/sensado.ino
@@ -0,0 +1,64 @@
+#include "LiSANDRA.h"
+
+
+#define LOCAL_ADDR  0x15
+#define DEST_ADDR   0x20
+#define NETKEY      {0xc0,0xd1,0xce}
+
+char tiempo[20];
+
+int SleepCnt = 0;
+
+void sleep_cpu_sec(uint8_t secs){
+    do{
+       RTC_SleepCPU();
+       SleepCnt++;
+    }while( SleepCnt==secs); 
+ }
+
+void setup() {
+    uint8_t net[3]=NETKEY;    
+    delay(3000);
+    Serial.begin(38400);
+    Serial.println("UART0: Inicializado");
+    
+    LEDs_Ini();    
+    Serial.println("Leds: Inicializados");
+    
+    LightSensor_Ini(); 
+    Serial.println("Light Sensor: Inicializado");
+    
+    HTsensor_Ini();
+    Serial.println("H&T Sensor: Inicializado");
+    
+    RadioLT_NetIni( net, LOCAL_ADDR, DEST_ADDR );   
+    Serial.println("Network: Inicializada");
+ 
+    RTC_Ini();
+    Serial.println("Real Tiempo Clock: Inicializado");
+      
+    RadioLT_Ini(); 
+    Serial.println("RF module: Inicializado");
+    
+}
+
+void loop() {
+  char msg[9]={"abcdefgh"};
+  sleep_cpu_sec(300); //Sensar cada 5 mins
+
+  /* sensar*/
+  unint16_t light_value = LightSensor_Read();
+  float heat_value = HTsensor_ReadTemperatureC();
+  float rh_value = HTsensor_ReadHumidity();
+  
+     /* conformar paquete */
+     /* enviar */
+     LEDs_Red_On();
+     delay(5);
+     LEDs_Red_Off();
+    
+     RadioLT_Send( msg ); 
+     RTC_GetTimeStr(tiempo);
+     Serial.println(tiempo);
+     Serial.flush();
+}
