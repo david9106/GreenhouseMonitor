08b792f968e75ed194c44f65ddcd3f9ec1847e4c
Dr.VAL <valeebp.blanco@gmail.com>
2016-09-30 17:27:38 -0700

    Subir json a DataStore

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
-j = '{"message": "hell"}' 
+jsonData = '{"message": "Frank"}'
+jsonToPython = json.loads(jsonData)
+#print (jsonToPython)
 
 class Censado(db.Model):
 	grados = db.FloatProperty(required=True)
@@ -20,8 +22,9 @@ class MainPage(webapp2.RequestHandler):
      def get(self):    
 		self.response.out.write(template.render('index.html', {}))
      def post(self):
-		shout = Shout( message=self.request.get('message'))
-		#shout = Shout(message=self.request.get(json.loads(j))
+		#shout = Shout( message=self.request.get('message'))
+		#shout.put()
+		shout = Shout(message=jsonToPython['message'])
 		shout.put()
 		self.redirect('/')
 
