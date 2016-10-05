e7e4f366bdf92b10917b63b6afb5b31964e57007
Dr.VAL <valeebp.blanco@gmail.com>
2016-10-05 11:47:33 -0700

    addIDLiSANDRA

--- DataBase/Censado.py
+++ DataBase/Censado.py
@@ -3,13 +3,16 @@ from google.appengine.ext import db
 
 class Censado(db.Model):
 	id = db.StringProperty(required=True)
+	id_LiSANDRA = db.StringProperty(required=True)
 	type = db.StringProperty(required=True)
 	value = db.FloatProperty(required=True)
 	when = db.DateTimeProperty(required=True)
 	
 	def addID(self, id):
 		self.id = id
-		
+	def addIDLiSANDRA(self, id_LiSANDRA)
+		self.id_LiSANDRA = id_LiSANDRA
+	
 	def addType(self, type):
 		self.type = type
 		
