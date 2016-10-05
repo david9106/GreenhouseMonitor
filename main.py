49cdbcb15c956cd1e56057a90ce7793fc9e16cea
Dr.VAL <valeebp.blanco@gmail.com>
2016-10-04 17:28:08 -0700

    lel

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import webapp2
+import socket
 import json
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
-jsonData = '{"message": "Frank"}'
+
+
+#print "received message:", data
+
+jsonData = '{"message": "Lile"}'
 jsonToPython = json.loads(jsonData)
-#print (jsonToPython)
+#jsonToPython = json.loads(data)
 
 class Censado(db.Model):
 	grados = db.FloatProperty(required=True)
@@ -17,15 +22,16 @@ class Censado(db.Model):
 class Shout(db.Model):
 	message = db.StringProperty(required=True)
 	when = db.DateTimeProperty(auto_now_add=True)
+	
+def addToDB():
 
 class MainPage(webapp2.RequestHandler):
      def get(self):    
 		self.response.out.write(template.render('index.html', {}))
      def post(self):
-		#shout = Shout( message=self.request.get('message'))
-		#shout.put()
-		shout = Shout(message=jsonToPython['message'])
-		shout.put()
+		#sens = Censado(grados=jsonToPython['temp'], luxes=jsonToPython['luz'], humedad=jsonToPython['hum'])
+		sens = Shout(message=jsonToPython['message'])
+		sens.put()
 		self.redirect('/')
 
 app = webapp2.WSGIApplication([
