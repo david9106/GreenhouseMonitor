f4a7dd6210abf6a8862a460d5684d6be5160f367
Rafael Karosuo <rafaelkarosuo@gmail.com>
2016-10-05 20:30:42 -0700

    Quitar ID, agregar GQL, quitar put()
    Quitar la funci[on que expone el asignar un ID, mejor que se genere automaticamente
    Quitar la funcion put y moverla a save_In_DB
    Arreglar get_All, tenia los valores de la otra BD de eventos
    Cambiar el paso de parametros a la cadena de GQL

--- DataBase/Censado.py
+++ DataBase/Censado.py
@@ -1,19 +1,10 @@
 from google.appengine.ext import db
 
 class Censado(db.Model):
-	id = db.StringProperty(required=True)
 	id_LiSANDRA = db.StringProperty(required=True)
 	type = db.StringProperty(required=True)
 	value = db.FloatProperty(required=True)
 	when = db.DateTimeProperty(required=True)
-		
-	def set_ID(self, id):
-	""" Agregar nuevo id de sensor """
-		try:
-			id = str(id)
-			self.id = id
-		except ValueError:
-			print("No es un ID permitido")
 
 	def set_LiSANDRA(self, id_LiSANDRA):
 	"""Agregar nuevo id de Modulo LiSANDRA """
@@ -40,21 +31,21 @@ class Censado(db.Model):
 			print("Valor del sensor debe ser flotante")
 			
 	#Funcion para guardar en Base de Datos	
-	def set_In_DB(self):
+	def save_In_DB(self):
 		self.put()
 		
 	def set_Time(self, when):
 	""" Agregar Fecha y tiempo en que se realizo la medicion """		
 		self.when = when
-		self.put()
 
-	def All(self):
+	def get_All(self):
 	"""Funcion que muestra todo lo que esta """
-		query_str = "SELECT * FROM Eventos"
+		query_str = "SELECT * FROM Censado"
 		return db.GqlQuery(query_str)
 
 	#Funcion para buscar censados entre fechas
 	def view_Date(self, date_1, date_2):
-		query_str = "SELECT * FROM Censado WHERE when >= DATETIME('-"+date_1+"') AND when <= DATETIME('-"+date_2+"')"
+		#query_str = "SELECT * FROM Censado WHERE when >= DATETIME('-"+date_1+"') AND when <= DATETIME('-"+date_2+"')"
+		query_str = "SELECT * FROM Censado WHERE when >= DATETIME(:date_1) AND when <= DATETIME(:date_2)"
 		#SELECT * FROM Shout WHERE when >= DATETIME('2013-09-29T09:30:20.00002-08:00') AND when <= DATETIME('2016-09-29T09:30:20.00002-08:00')
-		return db.GqlQuery(query_str)
+		return db.GqlQuery(query_str,date_1,date_2)
