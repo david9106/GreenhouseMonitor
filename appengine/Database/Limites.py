a22db1c28d3a560f4285582ee58e43ec44e72ee3
Dr.VAL <valeebp.blanco@gmail.com>
2016-10-15 23:07:15 -0700

    cambio de nombre alertas

--- appengine/Database/Limites.py
+++ appengine/Database/Limites.py
@@ -1,37 +1,30 @@
 from google.appengine.ext import db
 
 class Alertas(db.Model):
-	type = db.StringProperty()
+	type = db.Key()
 	max = db.FloatProperty()
 	min = db.FloatProperty()
 	
 	def set_Valor_Max(tipo_sensor,valor_Max):
 		"""Funcion que verifica si ya existe una alerta de un valor maximo de un tipo de sensado, si existe la actualiza si no crea una nueva"""
-		sensor_aux = Alertas.all().filter('type =',tipo_sensor).fetch(1)
+		sensor_aux = Alertas.get_or_insert(tipo_sensor)
 		if sensor_aux == None:
-			new_alert = Alertas()
-			new_alert.type = self.type
-			new_alert.max = valor_Max
-			new_alert.put()
+			sensor_aux.max = valor_Max
+			sensor_aux.put()
 		else:
-			old_alert = sensor_aux.key()
-			alert = Alertas.get(old_alert)
-			alert.max = valor_Max
-			alert.put()
+			sensor_aux.max = valor_Max
+			sensor_aux.put()
 		
 	def set_Valor_Min(tipo_sensor,valor_min):
 		"""Funcion que verifica si ya existe una alerta de un valor minimo de un tipo de sensado, si existe la actualiza si no crea una nueva"""
-		sensor_aux = Alertas.all().filter('type =',tipo_sensor).fetch(1)
+		sensor_aux = Alertas.get_or_insert(tipo_sensor)
 		if sensor_aux == None:
-			new_alert = Alertas()
-			new_alert.type = tipo_sensor
-			new_alert.max = valor_min
+			sensor_aux.max = valor_min
+			sensor_aux.put()
 		else:
-			old_alert = sensor_aux.key()
-			alert = Alertas.get(old_alert)
-			alert.max = valor_min
-			alert.put()
-			
+			sensor_aux.max = valor_min
+			sensor_aux.put()
+
 	def save_alert(self):
 		"""Guardar alerta creada en Base de Datos"""
 		self.put()
