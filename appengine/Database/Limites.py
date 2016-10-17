e7d4dbaa7a4749426ca5bb2208b908d0b16cc6bf
Dr.VAL <valeebp.blanco@gmail.com>
2016-10-16 17:46:00 -0700

    Agregado de propiedad id a Limites

--- appengine/Database/Limites.py
+++ appengine/Database/Limites.py
@@ -1,27 +1,32 @@
 from google.appengine.ext import db
 
 class Alertas(db.Model):
-	type = db.Key()
+	id_Lisandra = db.Key()
+	type = db.StringProperty()
 	max = db.FloatProperty()
 	min = db.FloatProperty()
 	
-	def set_Valor_Max(tipo_sensor,valor_Max):
+	def set_Valor_Max(id_Lisandra,tipo_sensor,valor_Max):
 		"""Funcion que verifica si ya existe una alerta de un valor maximo de un tipo de sensado, si existe la actualiza si no crea una nueva"""
-		sensor_aux = Alertas.get_or_insert(tipo_sensor)
+		sensor_aux = Alertas.get_or_insert(id_Lisandra)
 		if sensor_aux == None:
+			sensor_aux.type = tipo_sensor
 			sensor_aux.max = valor_Max
 			sensor_aux.put()
 		else:
+			sensor_aux.type = tipo_sensor
 			sensor_aux.max = valor_Max
 			sensor_aux.put()
 		
-	def set_Valor_Min(tipo_sensor,valor_min):
+	def set_Valor_Min(id_Lisandra,tipo_sensor,valor_min):
 		"""Funcion que verifica si ya existe una alerta de un valor minimo de un tipo de sensado, si existe la actualiza si no crea una nueva"""
-		sensor_aux = Alertas.get_or_insert(tipo_sensor)
+		sensor_aux = Alertas.get_or_insert(id_Lisandra)
 		if sensor_aux == None:
+			sensor_aux.type = tipo_sensor
 			sensor_aux.max = valor_min
 			sensor_aux.put()
 		else:
+			sensor_aux.type = tipo_sensor
 			sensor_aux.max = valor_min
 			sensor_aux.put()
 
@@ -29,8 +34,9 @@ class Alertas(db.Model):
 		"""Guardar alerta creada en Base de Datos"""
 		self.put()
 
-	def get_Max_Value(type):
+	def get_Max_Value(id_Lisandra,type):
 		"""Funcion que retorna el valor maximo del tipo de censado"""
+		#utilizar get()???
 		max_of = Alertas().all().filter('type =', type).fetch(1)
 		return max_of
 	
