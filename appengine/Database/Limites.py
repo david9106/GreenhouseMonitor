fdd3629b833753c7d965b0631333d19b84006cee
Dr.VAL <valeebp.blanco@gmail.com>
2016-10-17 10:29:09 -0700

    Propiedad identificador de censo agregada

--- appengine/Database/Limites.py
+++ appengine/Database/Limites.py
@@ -1,13 +1,14 @@
 from google.appengine.ext import db
 
 class Alertas(db.Model):
-	name_type = db.Key()
+	sens_id = db.Key()
+	name_type = db.String()
 	max = db.FloatProperty()
 	min = db.FloatProperty()
 	
-	def set_Valor_Max(tipo_sensor,valor_Max):
+	def set_Valor_Max(identificador,tipo_sensor,valor_Max):
 		"""Funcion que verifica si ya existe una alerta de un valor maximo de un tipo de sensado, si existe la actualiza si no crea una nueva"""
-		sensor_aux = Alertas.get_or_insert(tipo_sensor)
+		sensor_aux = Alertas.get_or_insert(identificador)
 		if sensor_aux == None:
 			sensor_aux.name_type = tipo_sensor
 			sensor_aux.max = valor_Max
@@ -15,9 +16,9 @@ class Alertas(db.Model):
 			sensor_aux.name_type = tipo_sensor
 			sensor_aux.max = valor_Max
 
-	def set_Valor_Min(tipo_sensor,valor_min):
+	def set_Valor_Min(identificador, tipo_sensor, valor_min):
 		"""Funcion que verifica si ya existe una alerta de un valor minimo de un tipo de sensado, si existe la actualiza si no crea una nueva"""
-		sensor_aux = Alertas.get_or_insert(tipo_sensor)
+		sensor_aux = Alertas.get_or_insert(identificador)
 		if sensor_aux == None:
 			sensor_aux.name_type = tipo_sensor
 			sensor_aux.max = valor_min
--- appengine/Handlers/LimitHandler.py
+++ appengine/Handlers/LimitHandler.py
@@ -1,14 +1,14 @@
 from Database import Limites
 
-def new_Alert(sensor_type, max_value, min_value)
+def new_Alert(identifier, sensor_type, max_value, min_value)
 	limit = Limites.Alertas()
-	limit.set_Valor_Max(sensor_type,max_value)
-	limit.set_Valor_min(sensor_type,min_value)
+	limit.set_Valor_Max(identifier, sensor_type, max_value)
+	limit.set_Valor_min(identifier, sensor_type, min_value)
 	limit.save_alert()
 	
-def get_Max_Value(type):
+def get_Max_Value(identifier):
 	"""Funcion que retorna el valor maximo del tipo de censado"""
-	max_of = Limites.Alertas.get_by_key_name(type)
+	max_of = Limites.Alertas.get_by_key_name(identifier)
 	if max_of:
 		return max_of.max
 	else:
@@ -16,7 +16,7 @@ def get_Max_Value(type):
 	
 def get_min_Value(type):
 	"""Funcion que retorna el valor minimo del tipo de censado"""
-	min_of = Limites.Alertas.get_by_key_name(type)
+	min_of = Limites.Alertas.get_by_key_name(identifier)
 	if min_of:
 		return min_of.min
 	else:
