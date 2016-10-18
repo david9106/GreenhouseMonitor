cd51d6698dae3ef90264d43184e76124069cd52d
Dr.VAL <valeebp.blanco@gmail.com>
2016-10-18 10:35:41 -0700

    Alerts parameter change

--- appengine/Database/Limites.py
+++ appengine/Database/Limites.py
@@ -5,24 +5,24 @@ class Alertas(db.Model):
 	max = db.FloatProperty()
 	min = db.FloatProperty()
 	
-	def set_Valor_Max(tipo_sensor,valor_Max):
+	def set_Valor_Max(self,valor_Max):
 		"""Funcion que verifica si ya existe una alerta de un valor maximo de un tipo de sensado, si existe la actualiza si no crea una nueva"""
-		sensor_aux = Alertas.get_or_insert(tipo_sensor)
+		sensor_aux = Alertas.get_or_insert(self)
 		if sensor_aux == None:
-			sensor_aux.name_type = tipo_sensor
+			sensor_aux.name_type = self
 			sensor_aux.max = valor_Max
 		else:
-			sensor_aux.name_type = tipo_sensor
+			sensor_aux.name_type = self
 			sensor_aux.max = valor_Max
 
-	def set_Valor_Min(tipo_sensor, valor_min):
+	def set_Valor_Min(self, valor_min):
 		"""Funcion que verifica si ya existe una alerta de un valor minimo de un tipo de sensado, si existe la actualiza si no crea una nueva"""
-		sensor_aux = Alertas.get_or_insert(tipo_sensor)
+		sensor_aux = Alertas.get_or_insert(self)
 		if sensor_aux == None:
-			sensor_aux.name_type = tipo_sensor
+			sensor_aux.name_type = self
 			sensor_aux.max = valor_min
 		else:
-			sensor_aux.name_type = tipo_sensor
+			sensor_aux.name_type = self
 			sensor_aux.max = valor_min
 
 	def save_alert(self):
