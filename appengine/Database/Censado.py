b24b2be8750f64c3374d7d5334d1df44c7beaf42
Dr.VAL <valeebp.blanco@gmail.com>
2016-10-18 11:54:43 -0700

    Alertas listo!

--- appengine/Database/Censado.py
+++ appengine/Database/Censado.py
@@ -1,69 +1,46 @@
 from google.appengine.ext import db
 import datetime
 
-from dateutil import parser #Temporal, just for date parsing
-
 class Censado(db.Model):
-	id_LiSANDRA = db.StringProperty()
-	type = db.StringProperty()
-	value = db.FloatProperty()
-	#when = db.DateTimeProperty(auto_now_add=True)
-	when = db.DateTimeProperty()
+	id_LiSANDRA = db.StringProperty(required=True)
+	type = db.StringProperty(required=True)
+	#value = db.StringProperty(required=True)
+	value = db.FloatProperty(required=True)
+	when = db.DateTimeProperty(auto_now_add=True)
 
-
-	def set_Time(self, new_date):
-		'''Temporal method to add dates, if it's not datetime, it tries to convert it'''
-		if isinstance(new_date, datetime.datetime):
-			self.when = new_date
-			return True
-		else:
-			try:
-				self.when = datetime.datetime.strptime(new_date, '%Y-%m-%d %H:%M:%S')
-			except ValueError:
-				print("Formato de fecha no valido, debe ser datetime o string")
-				return False
-	
-	
-	
 	def set_LiSANDRA(self, id_LiSANDRA):
-		"""Agregar nuevo id de Modulo LiSANDRA """
+	#Agregar nuevo id de Modulo LiSANDRA
 		try:
-			self.id_LiSANDRA = int(id_LiSANDRA)
-			return True
+			id_LiSANDRA = str(id_LiSANDRA)
+			self.id_LiSANDRA = id_LiSANDRA
 		except ValueError:
-			print("id_LiSANDRA debe ser ENTERO")
-			return False
+			print("ID de LiSANDRA no permitido")
 	
 	def set_Type(self, type):
-		""" Agregar tipo de la medicion hecha """		
+	#Agregar tipo de la medicion hecha
 		try:
 			type = str(type)
 			self.type = type
-			return True
 		except ValueError:
 			print("Tipo de censado no permitido")
-			return False
 
 	def set_Value(self, value):
-		""" Agregar el valor de la medicion realizada"""		
+	"""Agregar el valor de la medicion realizada"""
 		try:
 			value = float(value)
 			self.value = value
-			return True
 		except ValueError:
 			print("Valor del sensor debe ser flotante")
-			return False
 			
 	def save_In_DB(self):
-		"""Funcion para guardar en Base de Datos"""
+	"""Funcion para guardar en Base de Datos"""
 		self.put()
 		
 	def get_All(self):
-		"""Funcion que muestra todo lo que esta """
-		query_str = Censado.all()
-		return query_str
+	"""Funcion que muestra todo lo que esta"""
+		query_str = "SELECT * FROM Censado"
+		return db.GqlQuery(query_str)
 
-	def get_data_between_dates(date_1, date_2, type):
+	def get_Data(self, date_1, date_2, type):
 		"""Funcion para buscar censados entre fechas y tiempos"""
-		censados = Censado.all().filter('when >',datetime.datetime(date_1.year, date_1.month, date_1.day, date_1.hour, date_1.minutes)).filter('when <',datetime.datetime(date_2.year, date_2.month, date_2.day, date_2.hour, date_2.minutes)).filter('type =',type).fetch(None)
-		return censados
+		return Censado.all().filter('when >',datetime.datetime(date_1.year, date_1.month, date_1.day, date_1.hour, date_1.minutes)).filter('when <',datetime.datetime(date_2.year, date_2.month, date_2.day, date_2.hour, date_2.minutes)).filter('type =',type).fetch(None)
--- appengine/Database/Limites.py
+++ appengine/Database/Limites.py
@@ -5,19 +5,19 @@ class Alertas(db.Model):
 	max = db.FloatProperty()
 	min = db.FloatProperty()
 	
-	def set_type_sensor(self, sensor_type)
+	def set_type_sensor(self, sensor_type):
 		try:
 			self.type_sensor = str(sensor_type)
 		except ValueError:
 			print("The name of the sensor type must be a string")
 			
-	def set_max(self,max_Value)
+	def set_max(self,max_Value):
 		try:
 			self.max = float(max_Value)
 		except ValueError:
 			print("Max value must be a floting point number")
 	
-	def set_min(self,min_Value)
+	def set_min(self,min_Value):
 		try:
 			self.min = float(min_Value)
 		except ValueError:
