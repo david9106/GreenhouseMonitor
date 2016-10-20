b714d871703b0451eb82991dfb02294f4aecc574
David\DavidPC <a290206@uabc.edu.mx>
2016-10-19 19:35:49 -0700

    rpi con cambos de martin

--- WSN/rpi/sendToServer.py
+++ WSN/rpi/sendToServer.py
@@ -1,25 +1,32 @@
 import requests
 import json
+import time
 
-#Usar la ip correspondiente
-url = "http://192.168.1.74/getjson/catchJson.php"
-##url = "https://sensado-invernadero.appspot.com/set_sensors"
+url = "http://192.168.0.106/getjson/catchJson.php"
+#url = "https://sensado-invernadero.appspot.com/set_sensors"
+#url = "https://redsensoreslisandra.appspot.com"
 headers = {'Content-type':'application/json'}
-def sendJson(sensores):
-	#Se envia la informacion de los sensores al servidor
-	response = requests.post(url,json.dumps(sensores),timeout=5)
-	#Nota: Esta parte es solo para impresion
-	if response.status_code == 200:
-		data = response.json()
-		#Se muestra lo recibido por el servidor
-		print ("Servidor:")
-		d = json.loads(data)
-		print (d['temperatura'])
-		print (d['humedad'])
-		print (d['luz'])
-		print (d['CO2'])
-		print (d['fecha'])
-		print (d['hora'])
-	else:
-		print ("Hubo un error en la conexion")
-        
+
+def send(lista_sensores):
+        for sensor in lista_sensores:
+                sendJson(sensor)
+                time.sleep(1)
+                
+def sendJson(sensor):
+        j=json.dumps(sensor)
+        try:
+                #Se envia la informacion de los sensores al servidor
+                print("json: "+j)
+                response = requests.post(url,j,timeout=1)
+                print("Server response code: "+str(response.status_code))
+                if response.status_code == 200:
+                        print ("Enviado ...")
+                else:
+                        print ("Hubo un error en la conexion")
+        except requests.exceptions.Timeout:
+                print("Timeout ...")
+        except requests.exceptions.TooManyRedirects:
+                print("Demasiadas redirecciones ...")
+        except requests.exceptions.RequestException as e:
+                print(e)
+                sys.exit(1)
