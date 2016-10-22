8f4ae16688e81d02a650d44a6823bac9a70727f1
Dr.VAL <valeebp.blanco@gmail.com>
2016-10-21 17:17:30 -0700

    Add Phone class

---
+++ appengine/DataBase/Telefonos.py
@@ -0,0 +1,27 @@
+from google.appengine.ext import db
+
+class UserPhone(db.Model):
+	user_id = db.Key()
+	user_name = db.String()
+	user_phone = db.Integer()
+	
+	def set_userID(self, id):
+		try:
+			self.user_id = str(id)
+		except ValueError:
+			print("ID no permitido")
+	
+	def set_userName(self, name):
+		try:
+			self.user_name = str(name)
+		except ValueError:
+			print("Nombre no permitido")
+		
+	def set_userPhone(self, phone):
+		try:
+			self.user_phone = int(phone)
+		except ValueError:
+			print("El telefono debe de estar conformado por numeros enteros")
+	
+	def save_phone(self):
+		self.put()
