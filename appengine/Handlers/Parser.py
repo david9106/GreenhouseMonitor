2ebea900df80de5b7029228756e4d57fd12e6c39
Rafael_Karosuo <rafaelkarosuo@gmail.com>
2016-10-24 22:01:35 -0700

    Adding some changes from graficar

--- appengine/Handlers/Parser.py
+++ appengine/Handlers/Parser.py
@@ -1,26 +1,28 @@
 import webapp2
 import json
-import cgi
+import cgi		
+from Handlers import BDHandler #All the methods to access the DB
 
-sensor_data = None
-
-class Echo_parser(webapp2.RequestHandler):
-	messg = 0
-	def get(self):
-		if Echo_parser.messg is not 0:
-			self.response.write('Message received: ' + str(Echo_parser.messg))
-			Echo_parser.messg = 0;
-		else:
-			self.response.write('Haven\'t heard about you... ' + str(Echo_parser.messg))
+class JSON_parser(webapp2.RequestHandler):
+	'''Receive the sensor JSON objects from gateway, parses them, call the comparator module and DB module'''
+	def post(self):		
+		try:		
+			#Receive the object and decodes it
+			jdata = json.JSONDecoder().decode(cgi.escape(self.request.body))
 			
+			#Saves it on the database
+			if not BDHandler.alta_sensor(jdata["Tipo"].encode('utf-8'),jdata["Valor"].encode('utf-8'), jdata["Ubicacion"].encode('utf-8')): ##Just for tests, real timestamp is given by DB
+				print("Error saving on DB")
+		
+			#Comparar si el valor esta fuera de los limites
+			#if not Alertas.compararLimites(jdata["Tipo"],jdata["Valor"]):
+				#Alertas.enviar_mensaje(jdata["Tipo"],jdata["Valor"])
+				
+			self.response.write(json.dumps(jdata))
+		except (ValueError, TypeError):			
+			self.error(415) #Using 415 UNSUPPORTED MEDIA TYPE
+			self.response.write("Not a JSON object")
 
-
-class Json_parser(webapp2.RequestHandler):
-	def post(self):
-		jdata = json.loads(cgi.escape(self.request.body))
-		Echo_parser.messg = 1;
-		#self.redirect('/echo');
-		#jmessage = {'message':'Hello, what are you doing here?'}
-		self.response.write(json.dumps(jdata))
+			
 	def get(self):
 		self.response.write('Hello, what are you doing here?')
--- appengine/Main.py
+++ appengine/Main.py
@@ -1,9 +1,12 @@
 import webapp2
-from Handlers import Parser
+from Handlers import Parser,MainPage
 							
 app = webapp2.WSGIApplication([
-    ('/set_sensors',Parser.Json_parser),
-    ('/echo', Parser.Echo_parser)
+    ('/set_sensors',Parser.JSON_parser),
+    ('/get_csv', MainPage.CSV_provider),
+    ('/', MainPage.Graph_display),
+    ('/get_json', MainPage.JSON_provider),
+    ('/get_config', MainPage.Config_provider)
 ], debug=False)
 
 
