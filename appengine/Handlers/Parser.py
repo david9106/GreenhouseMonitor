9a83946d48bc03b052dcb4b19bd8629ede7b3efe
martingtz <martin.gutierrez@uabc.edu.mx>
2016-10-27 23:53:19 -0700

    integrado modulo alertas.py

--- appengine/Handlers/Parser.py
+++ appengine/Handlers/Parser.py
@@ -2,6 +2,9 @@ import webapp2
 import json
 import cgi		
 from Handlers import BDHandler #All the methods to access the DB
+from Handlers import LimitHandler
+from Handlers import Alertas
+from Handlers import PhoneHandler
 
 class JSON_parser(webapp2.RequestHandler):
 	'''Receive the sensor JSON objects from gateway, parses them, call the comparator module and DB module'''
@@ -9,14 +12,15 @@ class JSON_parser(webapp2.RequestHandler):
 		try:		
 			#Receive the object and decodes it
 			jdata = json.JSONDecoder().decode(cgi.escape(self.request.body))
-			
+			Alertas.compararLimites(jdata)
 			#Saves it on the database
 			if not BDHandler.alta_sensor(jdata["Tipo"].encode('utf-8'),jdata["Valor"].encode('utf-8'), jdata["Ubicacion"].encode('utf-8')): ##Just for tests, real timestamp is given by DB
 				print("Error saving on DB")
 		
 			#Comparar si el valor esta fuera de los limites
+			
 			#if not Alertas.compararLimites(jdata["Tipo"],jdata["Valor"]):
-				#Alertas.enviar_mensaje(jdata["Tipo"],jdata["Valor"])
+			#Alertas.enviar_mensaje(jdata["Tipo"],jdata["Valor"])
 				
 			self.response.write(json.dumps(jdata))
 		except (ValueError, TypeError):			
