69a6296ea35d02a965c951529640650c9728c17f
Osbel Alejandro Islas Silvas <osbel.islas@uabc.edu.mx>
2016-11-01 17:00:40 -0700

    Replace of id property to name property in configuration page to resovle request issue

--- appengine/Handlers/MainPage.py
+++ appengine/Handlers/MainPage.py
@@ -65,5 +65,5 @@ class Config_provider(webapp2.RequestHandler):
 class Phone_Config(webapp2.RequestHandler):
 	def post(self):
 		#phone = PhoneHandler.set_new_userPhone('1',self.request.get('check_phone_0'),self.request.get('phone_0'))
-		self.response.write(self.request.get('phone_0'))
+		self.response.write(self.request.get("phone_0"))
 	
