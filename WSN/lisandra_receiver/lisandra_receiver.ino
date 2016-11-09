4685733d5ae194ac0ab5991b5d5d5a67876c58dc
martin\omar <martin.gutierrez@uabc.edu.mx>
2016-11-09 13:40:31 -0800

    Added docstrings.
    Added docstring to sms.py,conversor.py,lisandra_transmitter.ino y
    lisandra_receiver.ino.

--- WSN/lisandra_receiver/lisandra_receiver.ino
+++ WSN/lisandra_receiver/lisandra_receiver.ino
@@ -1,65 +1,78 @@
+/**
+* @file lisandra_receiver.c
+* @author Gutierrez Martin,Blanco Erick V.,Gutierrez David F.,Islas Alejandro,G. Karosuo.
+* @date 11 Nov 2016
+* @brief Module used to receive frames from transmitter and transfer to gateway by serial port.
+*
+* @see https://github.com/david9106/IS-Repo-Equipo2/tree/master/WSN/lisandra_transmitter
+* @see https://github.com/david9106/IS-Repo-Equipo2/tree/master/WSN/rpi
+*/
+
 #include "LiSANDRA.h"
+
 #define LOCAL_ADDR  0x15
 #define DEST_ADDR   0x20
 #define NETKEY      {0xc0,0xd1,0xce}
 #define SENSORS_AMOUNT 4 
-uint16_t luz;
-uint16_t temp;
-uint16_t hum;
-uint16_t co2;
-uint8_t c=',';
 
+uint16_t temp;	/**< Variable used to save a sensor RAW measure temperature */
+uint16_t hum;	/**< Variable used to save a sensor RAW measure humidity */
+uint16_t light;		/**< Variable used to save a sensor RAW measure light */
+uint16_t co2=0xAA55;	/**< Variable used to save a sensor RAW measure carbon monoxide (CO2) */
+uint8_t net[3]=NETKEY;   /**< Variable used to save a NETKEY */
+RadioLT_DataPkt_t mypkt;	/**< Struct used to save receive data packet*/
 
-RadioLT_DataPkt_t mypkt;
-
-
-void setup() {
-  uint8_t net[3]=NETKEY;   
-	/*Inicializamos la comunicacion serial*/
-    Serial.begin(38400);
-	/*Inicializamos los leds*/
-    LEDs_Ini();    
-	/*Inicializamos el sensor de luz*/
-    LightSensor_Ini(); 
-	/*Inicializamos el sensor de humedad y temperatura*/
-    HTsensor_Ini();
-	/*Inicializamos el modulo RF*/
-    RadioLT_NetIni( net, LOCAL_ADDR, DEST_ADDR );  
-    RadioLT_Ini(); 
-	
-	/*Encendemos led (solo para saber si esta funcionando)*/
+/**@brief The setup() function is called when program starts. Is used to initialize functions.
+* The setup function will only run once, after each powerup or reset of the Lisandra board.
+* Initialize Serial,leds,sensors and radio.
+* @param None.
+*
+* @return None.
+*
+*/
+void setup() {  
+	Serial.begin(38400);
+	LEDs_Ini();    
+	LightSensor_Ini(); 
+	HTsensor_Ini();
+	RadioLT_NetIni( net, LOCAL_ADDR, DEST_ADDR );  
+	RadioLT_Ini(); 
 	LEDs_Grn_On(); 
 }
 
-
+/**@brief The loop() function does precisely what its name suggests, and loops consecutively, 
+* allowing your program to change and respond. 
+* The function checks if the packet received is a battery packet,if is ,checks content
+* if the content is battery low or battery good send a message to gateway(RPI) by serial port.
+* In case of measures packet, first decode the packet data and then sends to gateway(RPI)
+*
+* @param None.
+*
+* @return None.
+*
+*/
 void loop() {
-  /*Esperamos a recibir un paquete*/
- if( RadioLT_AvailablePkt() > 0 ){
-     /*Obtenemos el paquete*/
-           RadioLT_GetPkt( &mypkt );
-           
-     String str(mypkt.data);
+
+if( RadioLT_AvailablePkt() > 0 )
+{
+	RadioLT_GetPkt( &mypkt );
+	String str(mypkt.data);
      if(str == "BATL"){
-          
            Serial.print("BateriaBaja");
            Serial.print(',');
            Serial.print(mypkt.src);
            Serial.println();
-         
      }
      else if(str == "BATOK"){
            Serial.print("BateriaOK");
            Serial.print(',');
            Serial.print(mypkt.src);
            Serial.println();
-      
      }
-     else{
-		     
+     else{   
 		   RadioLT_GetPkt( &mypkt );
-           decodificarTrama(mypkt.data);
-           //Ajustar code
-           sendToRpi(mypkt.src,temp,hum,luz,co2);
+           decodeFrame(mypkt.data);
+           sendToRpi(mypkt.src,temp,hum,light,co2);
            LEDs_Ylw_On();
            delay(5);
            LEDs_Ylw_Off();
@@ -68,56 +81,60 @@ void loop() {
 
 }
 
-//Esta funcion se encarga de descomponer la trama recibida
-//en valores  enteros (16 bits ,2 bytes)
-  void decodificarTrama(char * trama){
-    uint8_t i=0;
-
-
-      
-      
+/**@brief The decodeFrame() decodes the packet (array of bytes) and parses to 
+* temperature,humidity,light co2 variables (each one of 2 bytes).
+*
+* @param Frame (packet received).
+*
+* @return None.
+*
+*/
+void decodeFrame(char * frame){
+    uint8_t i=0; 
       for(i=0;i<SENSORS_AMOUNT;i++){
         switch(i){
-      //Se obtiene  el byte alto (Nota: la trama lleva byte alto ,byte bajo,byte alto .....)
-      //de la trama y se le hace un cast a 8bits para pasarlo a ala variable que representara ese datos
-      //posteriormente se recorre 8bits esa misma variable para dejar libre los primeros 8 bits 
-      // y asignarlo el byte bajo.
-          case 0:
-      //byte alto
-            temp=trama[i];
-            temp=temp<<8;
-      //byte bajo
-            temp|= ((uint8_t)(trama[i+1]) );
 
-            //conformar arreglo
+          case 0:
+            temp=frame[i];
+            temp=temp<<8;
+            temp|= ((uint8_t)(frame[i+1]) );
             break;
           case 1:
-            hum=trama[i+1];
+            hum=frame[i+1];
             hum=hum<<8;
-            hum|= ((uint8_t)(trama[i+2]));
+            hum|= ((uint8_t)(frame[i+2]));
             break;
           case 2:
-            luz=trama[i+2];
-            luz=luz<<8;
-            luz|= ( (uint8_t)(trama[i+3]));
+            light=frame[i+2];
+            light=light<<8;
+            light|= ( (uint8_t)(frame[i+3]));
             break;
 
           case 3:
-            co2=trama[i+3];
+            co2=frame[i+3];
             co2=co2<<8;
-            co2|= ( (uint8_t)(trama[i+4]));
+            co2|= ( (uint8_t)(frame[i+4]));
             break;
           }
       }
 
 }
 
-
-void sendToRpi(uint16_t srcId,uint16_t temp,uint16_t hum,uint16_t luz,uint16_t co2){
-
-  /*code tipo,valor,ubicacion/tipo,valor,ubicacion/... salto de linea*/
-  //Se envia los datos a la raspberry como valores enteros y no como bytes
-      //ademas se incluye una coma entre cada valor para poder trabajarlos en la rpi
+/**@brief The sendToRpi() sends to gateway(RPI) all measures parsed from data
+* packet, in an especified format,each sensor separate by '/'. and every data
+* of sensor (source,type,measure) separate by ','.
+* Is nessesary a ' \ n ' to finish the message.
+*
+* @param param1 Source ID of packet
+* @param param2 RAW temperature measure from parsed packet.
+* @param param3 RAW humidity measure from parsed packet.
+* @param param4 RAW light measure from parsed packet.
+* @param param5 RAW CO2 measure from parsed packet.
+*
+* @return None.
+*
+*/
+void sendToRpi(uint16_t srcId,uint16_t temp,uint16_t hum,uint16_t light,uint16_t co2){
             
             Serial.print("Temperatura");
             Serial.print(',');
@@ -133,7 +150,7 @@ void sendToRpi(uint16_t srcId,uint16_t temp,uint16_t hum,uint16_t luz,uint16_t c
             Serial.print('/');
             Serial.print("Luz");
             Serial.print(',');
-            Serial.print(luz);
+            Serial.print(light);
             Serial.print(',');
             Serial.print(srcId);
             Serial.print('/');
@@ -143,6 +160,5 @@ void sendToRpi(uint16_t srcId,uint16_t temp,uint16_t hum,uint16_t luz,uint16_t c
             Serial.print(',');
             Serial.print(srcId);
             Serial.println();
-           
-  
+
   }
--- WSN/rpi/sms.py
+++ WSN/rpi/sms.py
@@ -1,14 +1,20 @@
+## @file sms.py
+# @brief This module is responsible sending phone alerts.
+# This module is used to send an alert to a phone number.
+# 
 from twilio.rest import TwilioRestClient
-#Script sms.py
+account_sid = "AC0b80488af39dcd381ac952934507ef69" # @var Your Account SID from www.twilio.com/console
+auth_token  = "617ce805fc5a06c6aabc9e3aaf3549bf"  # @var Your Auth Token from www.twilio.com/console
 
-account_sid = "AC0b80488af39dcd381ac952934507ef69" # Your Account SID from www.twilio.com/console
-auth_token  = "617ce805fc5a06c6aabc9e3aaf3549bf"  # Your Auth Token from www.twilio.com/console
-
+## @brief The function "sdnMsg" sends an alert.
+# Sends an alert to a phone number.
+# @param "telefono" Phone receiving the message
+# @return None
 def sendMsg(telefono):
 	client = TwilioRestClient(account_sid, auth_token)#protocolo para enviar msg
      
 	message = client.messages.create(body="BATERIA BAJA",
-	    to="+52"+telefono,    # Replace with your phone number
-	    from_="+16179970349") # Replace with your Twilio number
+	    to="+52"+telefono,    # @var Phone number
+	    from_="+16179970349") # @var Twilio number
 
 	print(message.sid+'\n')
