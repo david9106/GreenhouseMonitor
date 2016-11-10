4b43cbc86a12ed2b59d19e68d7072e0e9e8f5bae
Dr.VAL <valeebp.blanco@gmail.com>
2016-11-10 10:22:05 -0800

    minor changes

--- appengine/Templates/js/init_localstorage.js
+++ appengine/Templates/js/init_localstorage.js
@@ -1,8 +1,8 @@
 function mergeJSON(source1,source2){
-    /*
+    /**
      * Properties from the Souce1 object will be copied to Source2 Object.
      * Note: This method will return a new merged object, Source1 and Source2 original values will not be replaced.
-     * */
+     */
     var mergedJSON = Object.create(source2);// Copying Source2 to a new Object
 	
 	///Change "attrname" by date property, since it's the one that we want to ignore in case that it already exists
